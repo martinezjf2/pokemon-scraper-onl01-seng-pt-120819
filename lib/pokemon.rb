349ee1f5ad8024a3c7ab84540080a34cf9e880b2
Jeffrey Martinez <martinezjf2@gmail.com>
2020-03-06 03:47:19 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,8 @@
-require "pry"
+# filter_parameter_loggingrequire "pry"
 class Pokemon
   attr_accessor :name, :type, :db
   attr_reader :id
+  
   
   def initialize(hash)
     @name = hash[:name]
@@ -23,11 +24,13 @@ class Pokemon
   
   def self.find(id, db)
     # binding.pry
-    arr = db.execute("SELECT * FROM pokemon WHERE id = ?", [id])
-    new_pokemon = self.new(pokemon)
-    new_pokemon.id = pokemon[0][0]
-    new_pokemon.type = pokemon[0][2]
-    new_pokemon.name = pokemon[0][1]
+    hash = {}
+    arr = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    hash[:id] = arr[0]
+    hash[:type] = arr[2]
+    hash[:name] = arr[1]
+    hash[:db] = db
+    new_pokemon = self.new(hash)
     return new_pokemon
   end
 
